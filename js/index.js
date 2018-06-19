be96d3a36bbca5ec8b8539743081ba4a39886126
Philippe Coval <p.coval@samsung.com>
2018-06-19 14:43:08 +0200

    app: Reclocate scripts to js and use css
    Change-Id: I4abb1fe54f5be90853f1c215cab2d4bfc361967b
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+function log(arg)
+{
+  if (arg && arg.name && arg.message) {
+    var err = arg;
+    log("exception [" + err.name + "] msg[" + err.message + "]");
+  }
+  var text = "log: " + arg + "\n";
+  console.log(text);
+  document.form.console.value += text;
+  document.form.console.value.scrollTop = document.form.console.value.scrollHeight;
+}
+
+function evalSource()
+{
+  var source = document.form.source.value;
+  eval(source);
+}
+
 window.onload = function() {
     // TODO:: Do your initialization job
 
@@ -8,14 +26,5 @@ window.onload = function() {
                 tizen.application.getCurrentApplication().exit();
             } catch (ignore) {}
         }
-    });
-
-    // Sample code
-    var mainPage = document.querySelector('#main');
-
-    mainPage.addEventListener("click", function() {
-        var contentText = document.querySelector('#content-text');
-
-        contentText.innerHTML = (contentText.innerHTML === "Basic") ? "Tizen" : "Basic";
     });
 };

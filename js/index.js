327085c21744450b8b324988d88d3f54e8cb6ca2
Philippe Coval <p.coval@samsung.com>
2018-06-29 10:58:43 +0200

    pwa: Relocate serviceWorker in onload
    Change-Id: I77c88f9a1f7cad2d37501aff08cee11c21a928ab
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- js/index.js
+++ js/index.js
@@ -283,19 +283,23 @@ window.onload = function() {
     }
   });
 
+  // PWA
+  if ('serviceWorker' in navigator) {
+    try {
+      navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    } catch(e) {
+      console.log(e.message);
+    }
+  }
+
   // Autoconnect
   // TODO add settings page to disable (for debuging)
   app.main();
 };
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
 })();
 

eb0166db845b977a7844b7588df51c8fae47277a
Philippe Coval <p.coval@samsung.com>
2019-03-12 19:09:52 +0100

    aframe: createElements from type
    Change-Id: I41d5b3056961e2c507e566cf5beb73f809a29027
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- js/aframe/index.js
+++ js/aframe/index.js
@@ -8,66 +8,193 @@
  */
 
 var viewer = {};
-viewer.y = 0;
+viewer.count = 0;
+viewer.position = { x: -2 , y:-2 , z:0 };
 
-viewer.createViewUnknown = function(model)
+viewer.createPropertyElement = function (model, name)
 {
-  let view;
-  //viewer.el.setAttribute('text', 'value', value);
-
-  // https://github.com/gmarty/aframe-ui-components
-  // https://github.com/caseyyee/aframe-ui-widgets
-  // https://github.com/rdub80/aframe-gui
-  var newEl = document.createElement( "a-sphere" );
-  newEl.setAttribute( "position", "0 " + ( viewer.y += 0.3 ) + " 0" );
-  newEl.setAttribute( "radius", "0.1" );
-  newEl.setAttribute( "color", "lightblue" );
-  var readwrite = true; // for now fixed
-  if ( readwrite ){
-    var halo = document.createElement( "a-sphere" );
-    halo.setAttribute( "radius", "0.11" );
-    halo.setAttribute( "opacity", 0.2 );
-    newEl.appendChild( halo );
+  var self = this;
+  let property = model.properties[name];
+  let type = property.type;
+  let el = null;
+  console.log(property);
+  let view = document.createElement( "a-entity" );
+  view.setAttribute("text", "value", `\n${model.name}/${property.title} (${property.type})`);
+  view.setAttribute("text", "color", (property.readOnly) ? "#FFA0A0" : '#A0FFA0');
+  
+  var id = `widget-${viewer.count++}`;
+  console.log(id);
+  if (type === 'boolean') {
+    el = document.createElement( "a-entity" );
+    el.setAttribute("ui-toggle", "value", 0);
+    el.setAttribute("rotation", "90 0 0");
+  } else if (type === 'number') {
+    el = document.createElement( "a-cylinder" );
+    el.setAttribute("color", "#A0A0FF");
+    let number = 1; // TODO
+    el.setAttribute("height", "0.1" * number);
+    el.setAttribute("radius", "0.1");
+  } else {
+    el = document.createElement( "a-sphere" );
+    el.setAttribute("color", "#FF0000");
+    el.setAttribute("radius", "0.1");
   }
-  var nameEl = document.createElement( "a-entity" );
-  nameEl.setAttribute( "text", "value", `\n${model.name} (${model.type})` );
-  nameEl.setAttribute( "position", "0.7 0 0" );
-  newEl.appendChild( nameEl );
-  viewer.el.appendChild( newEl );
+  el.setAttribute("id", id);
+  el.setAttribute("position", "-1 0 0");
+  el.addEventListener('change', function(e) {
+    console.log('change:  ~~~ ');
+    if (e.detail) {
+      var payload = { on: (e.detail.value === 0 ) ? false : true};
+      app.put(property.links[0].href, payload, function(res, data) {
+	if (!res) {
+	  console.log(data);
+	}});
+    } else {
+      self.updateView(model, name, view);
+    }
+  });
+  view.setAttribute("id", `view-${id}`);
+  view.appendChild(el);
+  
+  if (false) {
+  if (!false &&  type === 'boolean')
+      setTimeout(function(){
+	console.log('emit{' + id);
+	var value = 0;
+	var view = document.querySelector(`#view-${id}`);
+	view.setAttribute('text', 'value', `TODO: ${id}=${value}`);
+	var el = document.querySelector(`#${id}`);
+	console.log(el);
+	console.log(el.getAttribute("value"));
+	console.log(el.getAttribute("ui-toggle"));
+	el.setAttribute('ui-toggle', "value", 0);
+	el.setAttribute('scale', [2,2,2]);
+	el.setAttribute('rotation', [0, 90, 0 ]);
+	console.log(el.getAttribute("ui-toggle"));
+	el.setAttribute('color', "#00FF00");
+	//el.emit('update', {value: 0});
+	console.log('emit}');
+    }, 4000);
 
+
+    if (!false &&  type === 'number')
+      setTimeout(function(){
+	//console.log('emit{' + id);
+	var el = document.querySelector(`#${id}`);
+	//console.log(el);
+	//console.log(el.getAttribute("color"));
+	el.setAttribute('scale', [ 1,2,3] );
+	el.setAttribute('color', "#00FF00");
+	el.emit('change');
+	//el.emit('change', {color: "#00FF00"});
+	console.log('emit}');
+    }, 4000);
+  }
   return view;
 }
 
-viewer.createViewOnOffSwitch = function (model)
-{
-  // TODO: implement this
-  return viewer.createViewUnknown(model);
-}
 
-viewer.createViewMultilevelSensor = function (model)
+viewer.updateView = function(model, name, view)
 {
-  // TODO: implement this
-  return viewer.createViewUnknown(model);
-}
+  var self = this;
+  var property = model.properties[name];
+  console.log(property);
+  var endpoint = property.links[0].href;
+  var url = localStorage['url'] + endpoint;
+  console.log(url);
+  let type = property.type;
+  let el = view.children[0];
+  console.log('~~~~~~');
+  console.log(el);
 
-viewer.createView = function(model)
+  app.get(url, function(err, data) {
+    if (!err) {
+      let text = view.getAttribute('text', 'value').value;
+      text = `\n${text}\n${data})`;
+      view.setAttribute("text", "value", text);
+
+      if (type === 'boolean') {
+	console.log('~~~ ' + data);
+	try {
+	  var value = JSON.parse(data)[name];
+	  value = (value) ? 1 : 0;
+	  var el = view.children[0];
+	  console.log(el.getAttribute('id'));
+	  console.log(el.getAttribute('ui-toggle'));
+	  el.setAttribute('ui-toggle', "value", value);
+	  console.log(el.getAttribute('ui-toggle'));
+	  el.emit('change', {value: value});
+	} catch(err) {
+	  console.log('TODO:' + err);
+	}
+      } else if (type === 'string') {
+	console.log('########'  + data );
+	let text = view.getAttribute('text', 'value').value;
+	text = `\n${text}\n${data})`;
+	view.setAttribute("text", "value", text);
+	//view.emit('change', {value: text});
+      }
+    }
+  });
+};
+
+
+viewer.appendThing = function (model)
 {
-  var view;
-  console.log(model.type);
-  if (model.type === "onOffSwitch"
-      || model.type === "dimmableColorLight") {
-    return this.createViewOnOffSwitch(model);
-  } else if (model.type === "multilevelSensor") {
-    return this.createViewMultilevelSensor(model);
-  } else {
-    return this.createViewUnknown(model);
+  var el = null;
+  console.log('createViewOnOffSwitch: ' + model.name);
+  for (name in model.properties){
+    var el = this.createPropertyElement(model, name);
+    try {
+      el.emit('change');
+    } catch(err) {
+      console.log('ignore: ' + err);
+    }
+    el.setAttribute("position", viewer.position.x + ' ' + viewer.position.y+ ' ' + viewer.position.z );
+    viewer.el.appendChild(el);
+    viewer.position.y +=.4;
   }
+  if (viewer.position.y > 2){
+    viewer.position.x += 2;
+    viewer.position.y = 0;
+  }
+  if (viewer.position.x > 2){
+    viewer.position.z -= 2;
+    viewer.position.x = 0;
+    viewer.position.y = 0;
+  }
+
+
+  return el;
 }
+
+
+viewer.thingQuery = function(url, token)
+{
+  let self = this;
+  console.log('log: query: ' + url);
+  app.get(url, function(err, data) {
+    if (err) throw err;
+    console.log(data);
+    let object = data && JSON.parse(data);
+    let items = Object.keys(object) || [];
+    let index;
+    for (index=0; index < items.length; index++) {
+      let model = object[items[index]];
+      model.local = {};
+      console.log(model);
+    };
+  });
+}
+
 
 viewer.query = function(url, token)
 {
   let self = this;
-  app.get(url, token, function(err, data) {
+  console.log('log: query: ' + url);
+  app.get(url, function(err, data) {
+    if (err) throw err;
+    console.log(data);
     let items = data && JSON.parse(data) || [];
     let index;
     for (index=0; index < items.length; index++) {
@@ -75,10 +202,10 @@ viewer.query = function(url, token)
       model.local = {};
       if (model.type === "thing") {
         console.log(model);
-        url = localStorage['url'] + model.href;
-        model.local.view = viewer.query(url, token);
+        url = localStorage['url'] + model.href + '/properties';
+        //model.local.view = viewer.thingQuery(url, token); //TODO
       } else {
-      model.local.view = self.createView(model);
+	model.local.view = self.appendThing(model);
       } 
     };
   });

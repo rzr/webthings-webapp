ca2eead477be1748cc5890e50d4301630c74aee4
Philippe Coval <p.coval@samsung.com>
2019-04-12 15:31:14 +0200

    tau: Refactor on app or profile base
    Change-Id: I32300ecc79f9f34580816588f575a244302c986a
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- profile/tau/js/index.js
+++ profile/tau/js/index.js
@@ -4,132 +4,25 @@
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-
 (function() {
   // 'use strict';
+  const viewer = app.viewer;
+  viewer.count = 0;
 
-  const app = {
-    isLoading: true,
-    datacontent: document.querySelector('.textarea'),
-  };
 
-  // TODO disable this if you want to hide log
-  app.log = console.log;
-
-  app.handleDocument = function(document) {
-    // TODO: https://github.com/mozilla-iot/gateway/pull/1142
-    // TODO: document.getElementById('token').textContent;
-    const xpath = '/html/body/section/div[2]/code/text()';
-    const iterator = document.evaluate(xpath, document, null,
-                                       XPathResult.ANY_TYPE, null);
-    const thisNode = iterator.iterateNext();
-    this.log(`token: ${thisNode.textContent}`); // TODO
-    localStorage.token = thisNode.textContent;
-  };
-
-  app.browse = function(url, callback) {
-    const self = this;
-    if (localStorage.token) {
+  viewer.log = !console.log || function(text) {
+    if (!app.devel) {
       return;
     }
-    this.log(`browse: ${url}`);
-    const delay = 50;
-    window.authCount = 0;
-    // TODO: https://github.com/mozilla-iot/gateway/pull/1149
-    window.addEventListener('message', function(ev) {
-      if (ev.data.message && ev.data.message.token) {
-        localStorage.token = ev.data.message.token;
-        window.authCount = 98;
-      }
-    });
-    window.authWin = window.open(url);
-    if (!window.authWin) {
-      throw `Cant open window: ${url}`;
-    }
-    window.interval = setInterval(function() {
-    // TODO: check if host alive using xhr
-      if (window.authCount > 60) {
-        window.clearInterval(window.interval);
-        if (window.authWin) {
-          window.authWin.close();
-        }
-        window.form.token.value = localStorage.token;
-        if (callback) {
-          callback();
-        }
-      }
-      window.authWin.postMessage({message: 'token'}, '*');
-      try {
-        url = (window.authWin &&
-               window.authWin.location &&
-               window.authWin.location.href) ?
-          window.authWin.location.href :
-          undefined;
-        if (url && (url.indexOf('code=') >= 0)) {
-          self.handleDocument(window.authWin.document);
-          window.authCount = 99;
-        } else {
-          window.authCount++;
-          self.log(`wait: ${url}`); // TODO
-        }
-      } catch (e) {
-        self.log(e.message);
-      }
-    }, delay);
+    console.log(text);
   };
 
-  app.get = function(endpoint, callback) {
-    const url = window.form.url.value + endpoint;
-    const token = localStorage.token;
-    const request = new XMLHttpRequest();
-    request.addEventListener('load', function() {
-      callback = callback || {};
-      callback(null, this.responseText);
-    });
-    this.log(url); // TODO
-    request.open('GET', url);
-    request.setRequestHeader('Accept', 'application/json');
-    request.setRequestHeader('Authorization', `Bearer ${token}`);
-    request.send();
-  };
 
-  app.put = function(endpoint, payload, callback) {
-    const url = window.form.url.value + endpoint;
-    const token = localStorage.token;
-    payload = JSON.stringify(payload);
-    this.log(url);
-    this.log(payload);
-    const request = new XMLHttpRequest();
-    request.addEventListener('load', function() {
-      callback = callback || {};
-      callback(null, this.responseText);
-    });
-    request.open('PUT', url);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.setRequestHeader('Accept', 'application/json');
-    request.setRequestHeader('Authorization', `Bearer ${token}`);
-    request.send(payload);
-  };
-
-  app.updateView = function(model, view) {
-    if (model.type === 'binarySensor' || model.type === 'onOffSwitch') {
-      const endpoint = model.properties.on.href;
-      this.get(endpoint, function(err, data) {
-        view.local.widget.checked = !!(JSON.parse(data).on);
-      });
-    } else if (model.type === 'multiLevelSensor') {
-      this.get(model.properties.level.href, function(err, data) {
-        view.local.widget.innerText = JSON.parse(data).level;
-      });
-    } else {
-      console.log(`TODO: implement ${model.type}`);
-    }
-  };
-
-  app.createBinarySensorView = function(li, model) {
-    const self = this;
+  viewer.createBinaryView = function(li, property, name) {
+    this.log(`createBinaryView:`);
+    this.log(property);
     li.setAttribute('class', 'ui-li-static ui-li-1line-btn1');
     const div = document.createElement('div');
     div.setAttribute('class', 'ui-btn.ui-btn-box-s ui-toggle-container');
@@ -142,13 +35,20 @@
     widget.setAttribute('data-tau-name', 'ToggleSwitch');
     widget.setAttribute('aria-disabled', 'false');
     widget.setAttribute('data-tau-bound', 'ToggleSwitch');
+    const endpoint = property.links[0].href;
+
     widget.addEventListener('click', function() {
-      widget.disabled = true;
-      self.get(model.properties.on.href, function(err, data) {
-        widget.disabled = false;
-        widget.checked = !!JSON.parse(data).on;
-      });
+      if (!property.readOnly) {
+        const payload = {};
+        payload[name] = !!(widget.checked);
+        app.put(endpoint, payload, function(res, data) {
+          if (res) {
+            console.error(data);
+          }
+        });
+      }
     });
+
     div.appendChild(widget);
     const handlerdiv = document.createElement('div');
     handlerdiv.setAttribute('class', 'ui-switch-handler');
@@ -157,47 +57,12 @@
     return li;
   };
 
-  app.createOnOffSwitchView = function(li, model) {
-    const self = this;
-    li.setAttribute('class', 'ui-li-static ui-li-1line-btn1');
-    const div = document.createElement('div');
-    div.setAttribute('class', 'ui-btn.ui-btn-box-s ui-toggle-container');
 
-    const widget = li.local.widget = document.createElement('input');
-    widget.setAttribute('type', 'checkbox');
-    // TODO: widget.tau = tau.widget.ToggleSwitch(widget);
-    widget.setAttribute('class', 'ui-toggle-switch');
-    widget.setAttribute('data-tau-built', 'ToggleSwitch');
-    widget.setAttribute('data-tau-name', 'ToggleSwitch');
-    widget.setAttribute('aria-disabled', 'false');
-    widget.setAttribute('data-tau-bound', 'ToggleSwitch');
-    const endpoint = model.properties.on.href;
-    widget.local = {};
-    widget.addEventListener('click', function() {
-      widget.disabled = true;
-      const wanted = (this.checked);
-      widget.local.interval = setTimeout(function() {
-        widget.disabled = false;
-      }, 1000);
-
-      self.log(`wanted: ${wanted}`);
-      self.put(endpoint, {on: wanted}, function(res, data) {
-        self.checked = !!(JSON.parse(data).on);
-        clearInterval(widget.local.interval);
-        widget.disabled = false;
-      });
-    });
-    div.appendChild(widget);
-    const handlerdiv = document.createElement('div');
-    handlerdiv.setAttribute('class', 'ui-switch-handler');
-    div.appendChild(handlerdiv);
-    li.appendChild(div);
-    return li;
-  };
-
-  app.createMultiLevelSensorView = function(li, model) {
-    const self = this;
+  viewer.createNumberView = function(li, property) {
     const widget = li.local.widget = document.createElement('button');
+    if (property.readOnly) {
+      widget.disable();
+    }
     // widget.tau = tau.widget.Button(widget); //TODO
     widget.setAttribute('class', 'ui-btn ui-inline');
     widget.setAttribute('data-tau-built', 'Button');
@@ -206,248 +71,149 @@
     widget.setAttribute('data-tau-bound', 'Button');
     widget.innerText = '?';
     widget.local = {};
-    widget.addEventListener('click', function() {
-      widget.local.interval = setTimeout(function() {
-        if (widget.disabled) {
-          self.put(model.properties.on.href, {on: true}, function(err, data) {
-            data = JSON.parse(data);
-            widget.disabled = false;
-            widget.innerText = (data.on) ? 'ON' : 'OFF';
-          });
-        }
-        widget.disabled = false;
-      }, 2000);
 
-      widget.disabled = true;
-      self.get(model.properties.on.href, function(err, data) {
-        data = JSON.parse(data);
-        if (!data.on) {
-          widget.innerText = 'OFF';
-        } else {
-          self.get(model.properties.level.href, function(err, data) {
-            data = JSON.parse(data);
-            clearInterval(widget.local.interval);
-            widget.disabled = false;
-            widget.innerText = (data) ? data.level : '?';
-          });
-        }
-      });
-    });
     li.setAttribute('class', 'ui-li-static ui-li-1line-btn1');
     const div = document.createElement('div');
     div.setAttribute('class', 'ui-btn.ui-btn-box-s ui-toggle-container');
     div.appendChild(widget);
     li.appendChild(div);
+
     return li;
   };
 
-  app.createView = function(model) {
-    const li = document.createElement('li');
-    li.tau = tau.widget.Listview(li);
-    li.value = model.name;
-    li.innerText = model.name;
-    li.local = {};
-    li.local.model = model;
 
-    model.local = {};
-    if (model.type === 'binarySensor') {
-      model.local.view = this.createBinarySensorView(li, model);
-    } else if (model.type === 'onOffSwitch' ||
-               model.type === 'dimmableColorLight') {
-      model.local.view = this.createOnOffSwitchView(li, model);
-    } else if (model.type === 'multiLevelSensor') {
-      model.local.view = this.createMultiLevelSensorView(li, model);
-    } else {
-      li.setAttribute('class', 'ui-li-static');
-      this.log(`TODO: implement ${model.type}`);
+  viewer.createPropertyView = function(model, name) {
+    const that = this;
+    const property = model.properties[name];
+    const type = property.type;
+    const semType = property['@type'];
+    const view = document.createElement('li');
+    view.tau = tau.widget.Listview(view);
+    view.value = property.title;
+    view.innerText = `${model.name}/${property.title}`;
+    view.local = {};
+    view.local.model = property;
+
+    property.local = {};
+    switch (property.type) { // TODO: mapping design pattern
+      case 'boolean':
+        property.local.view = that.createBinaryView(view, property, name);
+        break;
+      case 'number':
+        property.local.view = that.createNumberView(view, property);
+        break;
+      default:
+        view.setAttribute('class', 'ui-li-static');
     }
-    return li;
+    const id = `${this.count++}`;
+    that.log(`createPropertyElement: ${type}/${semType}`);
+    view.setAttribute('id', `view-${id}`);
+
+    return view;
   };
 
-  app.query = function(url) {
-    const self = this;
-    url = (url) || window.form.url.value;
-    this.log(`query: ${url}`);
-    this.get('/things', function(err, data) {
-      if (err || !data) {
-        throw err;
-      }
-      const items = data && JSON.parse(data) || [];
-      const list = document.getElementById('items');
-      list.innerHTML = '';  // Clean list
-      let listWidget;
-      for (let index = 0; index < items.length; index++) {
-        try {
-          const model = items[index];
-          const view = self.createView(model);
-          self.updateView(model, view);
-          list.appendChild(view);
-          listWidget = tau.widget.Listview(list);
-          listWidget.refresh();
-        } catch (e) {
-          self.log(e);
-        }
-      }
-    });
-  };
-
-  app.request = function(base_url) {
-    const self = this;
-    this.log(`request: ${base_url}`);
-    if (localStorage.token && localStorage.token.length) {
-      return self.query();
+  viewer.updateThingView = function(err, data, model) {
+    if (err) {
+      throw err;
     }
-    if (!base_url) {
-      throw 'URL needed';
-    }
-    let url = base_url;
-    url += '/oauth/authorize?';
-    url += `&client_id=${localStorage.client_id}`;
-    url += '&scope=/things:readwrite';
-    url += '&response_type=code';
-    if (!window.location.hostname) {
-      return this.browse(url, function() {
-        self.query();
-      });
-    } else {
-      const wurl = new URL(window.location);
-      const searchParams = new URLSearchParams(wurl.search);
-      const code = searchParams.get('code');
-      const isCallback = (localStorage.state === 'callback');
-      console.log(`isCallback${isCallback}`);
-      if (!code && !isCallback) {
-        this.log(url);
-        url += `&redirect_uri=${encodeURIComponent(document.location)}`;
-        localStorage.state = 'callback';
-        setTimeout(function() {
-          window.location = url;
-        }, 500);
-      } else if (code && isCallback) {
-        localStorage.state = 'token';
-        const auth_url = `${base_url}/oauth/token`;
-        const params = {
-          code: code,
-          grant_type: 'authorization_code',
-          client_id: localStorage.client_id,
-        };
-        const request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-          if (request.readyState == 4 && request.status == 200) {
-            localStorage.token = JSON.parse(request.responseText).access_token;
-            const pos = window.location.href.indexOf('?');
-            if (pos) {
-              const loc = window.location.href.substring(0, pos);
-              window.history.replaceState({}, document.title, loc);
-            }
-            self.query();
+    for (const name in data) {
+      const type = model.properties[name].type;
+      const semType = model.properties[name]['@type'];
+      const el = model.local[name].view;
+      // this.log(`// TODO: mapping design pattern:/prop/${name}:${type}`);
+      let widget;
+      switch (type) {
+        case 'boolean':
+          widget = el.getElementsByClassName('ui-toggle-switch')[0];
+          widget.checked = !!(data[name]);
+          break;
+        case 'number':
+        case 'integer':
+          widget = el.getElementsByClassName('ui-btn ui-inline')[0];
+          widget.innerText = data[name];
+          break;
+        case 'string':
+          if (semType === 'ColorProperty' || name === 'color') { // TODO
+            el.setAttribute('ui-button', 'baseColor', data[name]);
+          } else {
+            el.setAttribute('ui-rotary', 'value', data[name].length);
           }
-        };
-        request.open('POST', auth_url, true);
-        request.setRequestHeader('Content-type', 'application/json');
-        request.setRequestHeader('Accept', 'application/json');
-        request.setRequestHeader('Authorization', `Basic ${
-          window.btoa(`${localStorage.client_id
-          }:${localStorage.secret}`)}`);
-        request.send(JSON.stringify(params));
-      } else {
-        localStorage.state = 'disconnected';
+          break;
+        default:
+          this.log(`TODO: callback: ${name} : ${type}`);
       }
     }
   };
 
-  app.main = function() {
-    app.log(`main: ${localStorage.state}`);
-    app.log(`main: ${window.location.hostname}`);
-    // TODO: OAuth update ids here, URLs using file:// will copy from default
-    if (!localStorage.client_id || !localStorage.secret) {
-      if (!window.location.hostname) {
-        localStorage.client_id = 'local-token';
-        localStorage.secret = 'super secret';
-      } else {
-      // TODO: add GUI to overide default creds:
-        localStorage.client_id = window.location.hostname;
-        localStorage.secret = window.location.hostname;
-      }
-    }
-    try {
-      if (!localStorage.token) {
-        app.request(localStorage.url);
-      } else {
-        app.query();
-      }
-    } catch (err) {
-      this.log(err);
-    }
-  };
 
-  window.onload = function() {
-    const runButton = document.getElementById('run');
-    runButton.addEventListener('click', function() {
-      app.main();
-    });
-
-    const resetButton = document.getElementById('reset');
-    resetButton.addEventListener('click', function() {
-      document.form.url.value = '';
-      document.form.token.value = '';
-      localStorage.clear();
-      app.log('token forgotten (need auth again)');
-    });
-
-    const aboutButton = document.getElementById('about');
-    aboutButton.addEventListener('click', function() {
-      window.open('README.md');
-    });
-
-    const urlInput = document.getElementById('url');
-    if (localStorage.url && localStorage.url.length) {
-      window.form.url.value = localStorage.url;
-    } else {
-      window.form.value = 'http://gateway.local:8080';
-    }
-    urlInput.addEventListener('change', function() {
-      this.value = this.value.replace(/\/$/, '');
-      localStorage.url = this.value;
-    });
-
-    const tokenInput = document.getElementById('token');
-    if (localStorage.token && localStorage.token.length) {
-      window.form.token.value = localStorage.token;
-    }
-    tokenInput.addEventListener('change', function() {
-      this.value = this.value.replace(/\/$/, '');
-      localStorage.token = this.value;
-    });
-
-    // add eventListener for tizenhwkey
-    document.addEventListener('tizenhwkey', function(e) {
-      if (e.keyName === 'back') {
-        try {
-          tizen.application.getCurrentApplication().exit();
-        } catch (ignore) {
-          self.log(ignore);
-        }
-      }
-    });
-
-    // PWA
-    if ('serviceWorker' in navigator) {
+  viewer.appendThing = function(model) {
+    const that = this;
+    const view = null;
+    let propertyName = null;
+    that.log(`appendThing: ${model.type}`);
+    this.root = document.getElementById('items');
+    model.local = {};
+    for (propertyName in model.properties) {
       try {
-        navigator.serviceWorker.register('service-worker.js')
-          .then(function(registration) {
-            console.log('ServiceWorker registration successful with scope: ',
-                        registration.scope);
-          }, function(err) {
-            console.log('ServiceWorker registration failed: ', err);
-          });
-      } catch (e) {
-        console.log(e.message);
+        const el = this.createPropertyView(model, propertyName);
+        this.root.appendChild(el);
+        model.local[propertyName] = {view: el};
+      } catch (err) {
+        console.error(`ignore: ${err}`);
       }
     }
 
-    // Autoconnect
-    // TODO add settings page to disable (for debuging)
-    app.main();
+    app.poll(model, function(err, data) {
+      that.updateThingView(err, data, model);
+    });
+    app.listenThing(model, function(err, data) {
+      that.updateThingView(err, data, model);
+    });
+
+    return view;
   };
+
+
+  viewer.handleResponse = function(err, data) {
+    const that = viewer;
+    if (err || !data) {
+      console.error(err);
+      throw err;
+    }
+    let model = data;
+
+    if (typeof data === 'string' && data) {
+      model = data && JSON.parse(data);
+    }
+    if (Array.isArray(model)) {
+      let index;
+      for (index = 0; index < model.length; index++) {
+        that.handleResponse(err, model[index]);
+      }
+    } else {
+      this.appendThing(model);
+    }
+  };
+
+
+  viewer.query = function(endpoint) {
+    if (!endpoint) {
+      endpoint = localStorage.endpoint;
+    }
+    app.get(endpoint, viewer.handleResponse);
+  };
+
+
+  viewer.start = function() {
+    this.log(`start: ${localStorage.url}`);
+    if (!localStorage.url) {
+      console.warn('Gateway token unset');
+      window.location = app.loginUrl;
+    } else {
+      this.query();
+    }
+  };
+  viewer.start();
 })();
+
 
